17973113b5320f7628be2aacd79c021328368d46
Muntasir Chowdhury Mridul <54744758+Mrichigo-crypto@users.noreply.github.com>
2020-10-03 22:29:57 +0600

    Enemy Behaviour complete

---
+++ Maze Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform[] _wayPoints;
+    [SerializeField] private float _enemySpeed;
+    [SerializeField] private float _breakTime = 0.3f;
+    
+    private Transform _currentWayPoint;
+    private bool _goingForward;
+    private int _size;
+    private int _wpIndx;
+
+    void Start()
+    {      
+        _size = _wayPoints.Length - 1;
+    
+    }
+
+  
+    void Update()
+    {
+        if(transform.position == _wayPoints[0].position)
+         {
+             StartCoroutine(Break());
+            _wpIndx = 1;
+            _currentWayPoint = _wayPoints[_wpIndx];
+            _goingForward = true;
+         }
+
+        else if(transform.position == _wayPoints[_wpIndx].position && _wpIndx != _size)
+         {
+            if(_goingForward)
+               {
+                  
+                  _wpIndx += 1;
+                  _currentWayPoint = _wayPoints[_wpIndx];
+               }
+            else
+               {
+                  
+                 _wpIndx -= 1;
+                 _currentWayPoint = _wayPoints[_wpIndx];
+               }
+         } 
+
+         else if(transform.position == _wayPoints[_size].position)
+           {
+              StartCoroutine(Break());
+             _wpIndx -=1;
+             _currentWayPoint = _wayPoints[_wpIndx];
+              _goingForward = false;
+           }
+
+       transform.position = Vector2.MoveTowards(transform.position, _currentWayPoint.position, _enemySpeed * Time.deltaTime);
+    }
+
+    IEnumerator Break()
+     {
+       yield return new WaitForSeconds(_breakTime);
+     }
+       
+}
---
+++ Maze Game/Assets/Scripts/EnemyCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollider : MonoBehaviour
+{
+
+   [SerializeField] private GameObject _player;
+   
+
+    
+   void OnTriggerEnter2D(Collider2D other)
+       {
+         Debug.Log(other.gameObject.name);
+           if(other.gameObject.name == "Player")
+            {
+              Destroy(_player , 0.2f);
+            }
+       }
+}
