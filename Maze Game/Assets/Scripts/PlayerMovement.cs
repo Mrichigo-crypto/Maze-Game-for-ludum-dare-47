b1bf7134a84ed2ecd3965264903664ccf54a1705
Muntasir Chowdhury Mridul <54744758+Mrichigo-crypto@users.noreply.github.com>
2020-10-03 20:10:47 +0600

    Basic Movement And 1st level design done

---
+++ Maze Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private float _vInp;
+    private float _hInp;
+    [SerializeField] private float _speed;
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        _hInp = Input.GetAxis("Horizontal");
+        _vInp = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+      _rb.velocity = new Vector2(_hInp * _speed , _vInp * _speed);
+    }
+}
---
+++ Maze Game/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+
+   [SerializeField] private float _rotSpeed = 3f;
+  
+   
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+       float inp = Input.GetAxis("Horizontal");
+       float inpVer = Input.GetAxis("Vertical");
+ 
+       if(inp > 0.1f)
+       {
+        
+         var right = Quaternion.Euler(0,0,-90);
+         transform.rotation = Quaternion.Slerp(transform.rotation , right, Time.deltaTime * _rotSpeed);
+       }
+    
+      else if(inp < -0.1f)
+      {
+         var left = Quaternion.Euler(0,0,90);
+         transform.rotation = Quaternion.Slerp(transform.rotation , left , Time.deltaTime * _rotSpeed);
+      }
+        
+      if(inpVer > 0.1f)
+       {
+          var up = Quaternion.Euler(0,0,0);
+         transform.rotation = Quaternion.Slerp(transform.rotation , up, Time.deltaTime * _rotSpeed);
+       }
+ 
+       else if(inpVer < -0.1f)
+      {
+         var down = Quaternion.Euler(0,0,180);
+         transform.rotation = Quaternion.Slerp(transform.rotation , down , Time.deltaTime * _rotSpeed);
+      }
+
+         
+    }
+} 
